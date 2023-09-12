41ae97c6228bd731cbdb221360bc0eb1408c3882
John Ruiz <50649703+johnruiz17@users.noreply.github.com>
2023-09-12 14:43:54 -0700

    created a search bar component

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import SearchBar from './components/SearchBar';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>PARKING FINDER!!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<Text>PARKING FINDER!!</Text>
+			<SearchBar />
+			<StatusBar style='auto' />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center'
+	}
 });
